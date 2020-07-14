ae0f46eb50b22df32217437a738a6d58a3ac6b77
Anely <an.dusaeva@gmail.com>
2020-07-14 15:17:35 +0300

    Добавлены классы для работы с json

---
+++ src/main/java/jsonParser/Objects.java
@@ -0,0 +1,41 @@
+package jsonParser;
+
+public class Objects {
+    private int class_id;
+    private String name;
+    private Relative_coordinates relative_coordinates;
+    private float confidence;
+
+
+    public int getClass_id() {
+        return class_id;
+    }
+
+    public void setClass_id(int class_id) {
+        this.class_id = class_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Relative_coordinates getRelative_coordinates() {
+        return relative_coordinates;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getConfidence() {
+        return confidence;
+    }
+
+    public void setConfidence(float confidence) {
+        this.confidence = confidence;
+    }
+
+    public void setRelative_coordinates(Relative_coordinates relative_coordinates) {
+        this.relative_coordinates = relative_coordinates;
+    }
+}
