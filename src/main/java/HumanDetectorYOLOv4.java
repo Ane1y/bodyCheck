316a66a206dd56b73c3268135235f7d45a3bc5d2
somn117 <55783463+somn117@users.noreply.github.com>
2020-07-15 18:23:44 +0300

    добавлен почти реальный рост человека
    добавлен почти реальный рост человека

--- src/main/java/HumanDetectorYOLOv4.java
+++ src/main/java/HumanDetectorYOLOv4.java
@@ -14,22 +14,25 @@ public class HumanDetectorYOLOv4 {
     private File image;
 
     private Frame[] resultObjects;
-
+double imgHumanHeight;
     public HumanDetectorYOLOv4(File file) throws IOException {
         image = file;
-        ProcessBuilder builder = new ProcessBuilder("darknet/darknet.exe",
-                "detector", "test", "darknet/cfg/coco.data",
-                "darknet/cfg/yolov4.cfg",
-                "darknet/cfg/yolov4.weights", "-ext_output", "-dont_show", "-out", resultFile.getCanonicalPath(), file.getCanonicalPath());
-        builder.redirectErrorStream(true);
+
+        ProcessBuilder builder = new ProcessBuilder("C:/Programming/darknet-master/build/darknet/x64/darknet.exe",
+                "detector", "test", "C:/Programming/darknet-master/cfg/coco.data",
+                "C:/Programming/darknet-master/cfg/yolov4.cfg",
+                "C:/Programming/darknet-master/build/darknet/yolov4.weights", "-ext_output",
+                "-dont_show", "-out", "result.json", Main.path);
+         builder.redirectErrorStream(true);
         Process process = builder.start();
         try (BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
             String line;
             while ((line = br.readLine()) != null) {
                 System.out.println(line);
             }
-            builder.command("imgs/180.jpg");
+            builder.command(Main.path);
             process.waitFor();
+
         } catch(IOException | InterruptedException e) {
           e.printStackTrace();
         }
@@ -38,6 +41,8 @@ public class HumanDetectorYOLOv4 {
 
         Gson gson = new Gson();
         resultObjects = gson.fromJson(new FileReader(resultFile.getCanonicalPath()), Frame[].class);
+
+
     }
 
     /**
@@ -88,8 +93,25 @@ public class HumanDetectorYOLOv4 {
             frame = new Rect((int) ((rc.getCenter_x() - rc.getWidth() / 2) * img.getWidth()),
                     (int) ((rc.getCenter_y() - rc.getHeight() / 2) * img.getHeight()),
                     (int) (rc.getWidth() * img.getWidth()),
-                    (int) (rc.getHeight() * img.getHeight())
-            );
+                    (int) (rc.getHeight() * img.getHeight()));
+            if(name=="book"){
+                //String command = "python /c start python C:/Users/Somn117/PycharmProjects/DetectRect/q.py";
+               // Process p = Runtime.getRuntime().exec(command);
+                String prg = "import sys";
+                BufferedWriter out = new BufferedWriter(new FileWriter("C:/Programming/DetectRect/q.py"));
+                out.write(prg);
+                out.close();
+                Process p = Runtime.getRuntime().exec("python C:/Programming/DetectRect/q.py");
+                BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+                String ret = in.readLine();
+                System.out.println("value is : "+ret);
+            }
+            if (name=="person"){
+                imgHumanHeight=frame.height;
+                 File result = new File("C:/Programming/result/result.txt");
+                 HeightCalculate hc= new HeightCalculate(result,imgHumanHeight);
+                 System.out.print(hc.realHumanHeight);
+            }
         } catch(NullPointerException e) {
             System.out.print(name + " не найден");
         } catch(IOException e) {
@@ -98,7 +120,10 @@ public class HumanDetectorYOLOv4 {
         return frame;
     }
     public Rect detectBook() {
-        return detectObject("book");
+       // var book = detectObject("book");
+
+            return(detectObject("book"));
+
     }
     public Rect detectHuman() throws IOException {
         return detectObject("person");
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,36 @@
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
 
 import java.io.File;
 import java.io.IOException;
 
 public class Main {
+    public static String path="C:/Programming/imgs/sanyaandlove.jpg";
     public static void main(String[] args) {
         nu.pattern.OpenCV.loadLocally();
        /* File folder = new File("../imgs/");
         File[] listOfFiles = folder.listFiles();*/
     //new HumanDetector(listOfFiles);
  //   new HOGHumanDetector(listOfFiles);
-        File img = new File("imgs/175.jpg");
+        File img = new File(path);
+
         try {
             HumanDetectorYOLOv4 hd = new HumanDetectorYOLOv4(img);
-            hd.detectHuman();
-            hd.detectBook();
+           hd.detectHuman();
+          //  hd.detectBook();
+            Mat image_roi = new Mat(Imgcodecs.imread(img.getAbsolutePath()), hd.detectBook());
+         //   Mat image_roi2 = new Mat(Imgcodecs.imread(img.getAbsolutePath()), hd.detectHuman());
+            Imgcodecs.imwrite("C:/Programming/imgs/cropimage.jpg",image_roi);
         } catch (IOException e) {
             e.printStackTrace();
         }
+      //  File cropimg = new File("C:/Programming/imgs/cropimage.jpg");
+      /*  try {
+            DetectRectangle dr = new DetectRectangle(cropimg);
+        } catch(IOException e) {
+            e.printStackTrace();
+        }*/
     }
 }
