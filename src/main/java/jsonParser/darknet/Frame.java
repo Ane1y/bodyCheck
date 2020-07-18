5d7fa4dfba11b78e99f670684b1226a4c6c5f847
Ane1y <46869112+Ane1y@users.noreply.github.com>
2020-07-18 15:14:16 +0300

    я создал темный интернет, а что в этой жизни сделал ты

---
+++ src/main/java/jsonParser/darknet/Frame.java
@@ -0,0 +1,33 @@
+package jsonParser.darknet;
+
+import java.util.List;
+
+public class Frame {
+    private int frame_id;
+    private String filename;
+    private List<Objects> objects = null;
+
+    public int getFrame_id() {
+        return frame_id;
+    }
+
+    public void setFrame_id(int frame_id) {
+        this.frame_id = frame_id;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public List<Objects> getObjects() {
+        return objects;
+    }
+
+    public void setObjects(List<Objects> objects) {
+        this.objects = objects;
+    }
+}
